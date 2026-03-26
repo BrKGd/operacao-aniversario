e790f6c4c0b818e8b28333d083e5f243e0ee3acb
Gleidson Silva <gleidson.silva_aut@Outlook.com>
2026-03-26 08:44:57 -0300

    first commit

--- vite.ts
+++ vite.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/operacao-aniversario/',
+  
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
